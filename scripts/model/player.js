57612c0afcde32a1e6f290a86355378bf710babd
Washipp <silas.meier@bluewin.ch>
2017-10-10 20:05:02 +0200

    Added first files, nothing to see here

---
+++ scripts/model/player.js
@@ -0,0 +1,7 @@
+var player = {
+    isPlaying: true,
+    score: 0,
+    currentColor: 'white',
+    xPos: 0,
+    yPos: 0
+};
---
+++ scripts/model/rectangle.js
@@ -0,0 +1,7 @@
+var rectangle = {
+    rectColor: 'white',
+    rectX: 0,
+    rectY: 0,
+    rectWidth: 0,
+    rectHeight: 0
+};
